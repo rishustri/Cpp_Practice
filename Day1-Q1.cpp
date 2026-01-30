64e8a6d98206f73665ec541298e53c71a0643b9d
ApnaCollege <vaiibhavisingh855@gmail.com>
2026-01-30 23:09:27 +0530

    Added multiplication table program (Day1-Q1)

---
+++ Day1-Q1.cpp
@@ -0,0 +1,13 @@
+// Write a program to print the multiplication table of a number//
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    for (int i=1; i<=10; i++){
+        cout<< n <<" * " << i << " = " << n*i << endl;
+    }
+    return 0;
+}
