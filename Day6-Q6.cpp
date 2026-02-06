3f4b167eb4d176cec3bde5303cc50872ad9a4f60
ApnaCollege <vaiibhavisingh855@gmail.com>
2026-02-06 19:50:28 +0530

    Calculate the area of circle

---
+++ Day6-Q6.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    float r;
+    cout<<"Enter the radius of the circle: ";
+    cin>>r;
+    float area=3.14*r*r;
+    cout<<"Area="<<area;
+    return 0;
+
+}
